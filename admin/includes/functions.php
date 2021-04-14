4c91af782b5957432c567b1eb07575559a26283d
LucioSaldivar <manuelsaldivar310@gmail.com>
2021-04-14 12:46:37 -0500

    Sessions Class

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -6,8 +6,14 @@ function classAutoLoader($class)
     $the_path = "includes/{$class}.php";
 
     if(is_file($the_path) && !class_exists($class)){
-        include $the_path;
+        require_once($the_path);
+    } else {
+        die("This file name {$class}.php not found");
     }
 
     spl_autoload_register('classAutoLoader');
+}
+
+function redirect($location){
+    header("Location: {$location}");
 }
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -31,11 +31,28 @@ class User
         $result_set = $database->query($sql);
         $the_object_array = array();
 
-        while($row = mysqli_fetch_array($result_set)){
+        while ($row = mysqli_fetch_array($result_set)) {
             $the_object_array[] = self::instantation($row);
         }
 
         return $the_object_array;
+    }
+
+    public static function verify_user($username,$password)
+    {
+        global $database;
+
+        $username = $database->escape_string($username);
+        $password = $database->escape_string($password);
+
+        $sql = "SELECT * FROM users WHERE ";
+        $sql .= "username = '{$username}' ";
+        $sql .= "AND password = '{$password}' ";
+        $sql .= "LIMIT 1";
+
+        $the_result_array = self::find_this_query($sql);
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
     }
 
     public static function instantation($the_record)
